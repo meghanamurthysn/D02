8ecc23413c4c4b5094e183492fa73899810d0643
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-04 04:47:19 +0530

    HW02_ch05_ex00_v01

--- HW02/HW02_ch05_ex00.py
+++ HW02/HW02_ch05_ex00.py
@@ -8,13 +8,10 @@
 # Write your functions below:
 # Body
 
-
-
-
-
-
-
-
+def do_n(func,n):
+	if(n>0):
+		func()
+		do_n(func,n-1)
 
 # Write your functions above:
 def print_hello():
@@ -25,9 +22,8 @@ def main():
     When complete have one function call in this function:
     do_n(print_hello, 10)
     """
-    print_hello() # replace this with do_n(print_hello, 10)
-
-
+    do_n(print_hello, 10)
+    # print_hello() # replace this with do_n(print_hello, 10)
 
 if __name__ == "__main__":
     main()
